fb20191b6bfcb8815025df3d183daffe00a2f721
Michal Kahle <michalkahle@gmail.com>
2018-04-16 16:27:30 +0200

    Slightly simplified the install script.

--- install.sh
+++ install.sh
@@ -13,13 +13,8 @@ function copy {
 	echo -e "\e[92mcopied\e[0m: $1 to $2"
 }
 
-function remove {
-	sudo rm -f $1
-	echo -e "\e[91mremoved\e[0m: $1"
-}
-
 function symlink {
-	sudo ln -s $1 $2
+	sudo ln -sf $1 $2
 	echo -e "\e[93mcreated symlink\e[0m"
 	echo "target: $1"
 	echo "link name: $2"
@@ -32,9 +27,6 @@ createBackupIfNecessary "/usr/share/X11/xkb/rules/base.xml"
 copy "cz" 	"/usr/share/X11/xkb/symbols/cz"
 copy "base.lst" "/usr/share/X11/xkb/rules/base.lst"
 copy "base.xml" "/usr/share/X11/xkb/rules/base.xml"
-
-remove "/usr/share/X11/xkb/rules/evdev.lst"
-remove "/usr/share/X11/xkb/rules/evdev.xml"
 
 symlink "/usr/share/X11/xkb/rules/base.lst" "/usr/share/X11/xkb/rules/evdev.lst"
 symlink "/usr/share/X11/xkb/rules/base.xml" "/usr/share/X11/xkb/rules/evdev.xml"
